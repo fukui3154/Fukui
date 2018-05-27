0d775d1fe6c9d6cca43622662e5f302a1ca554b9
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-05-27 11:54:24 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
+
   get '/users', to:'users#index'
   get '/users/new', to:'users#new'
-  get '/users/create' to:'users#create'
-  get '/users/show' to:'users#show'
-  get '/users/edit' to:'users#edit'
-  get '/users/update' to:'users#update'
-  get '/users/destroy' to:'users#destroy'
-  
+  get '/users/create', to:'users#create'
+  get '/users/show', to:'users#show'
+  get '/users/edit', to:'users#edit'
+  get '/users/update', to:'users#update'
+  get '/users/destroy', to:'users#destroy'
+
 end
