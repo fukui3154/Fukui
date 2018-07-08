cfbe677a7ef4068b635d3b73491e8d7b09621475
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-07-08 20:24:39 +0900

    add new form to create users data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
 	def index
+		@users = User.all
 	end
 
 	def new
 	end
 
 	def create
-		@name = params[:name]
-		@age = params[:age]
-		@address = params[:address]
+		User.create(name: params[:name], age: params[:age], address: params[:address], hobby: params[:hobby], birth: params[:birth])
 	end
 
 	def show
